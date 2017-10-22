b3090909ac1f72ad2f5361af94ec63f17398f82c
Douglas Lowder <douglas@lowder.io>
2017-10-21 18:04:39 -0700

    Adds ES6 arrow functions

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 
 const uuid = require('uuid');
 
-module.exports = function(options) {
-    return function(req, res, next) {
+module.exports = (options) => {
+    return (req, res, next) => {
         options = options || {};
         options.header = options.header || 'X-Request-Tag';
         if (!options.provider || !(typeof options.provider.generate === 'function')) {
             options.provider = {
-                generate:function() {
+                generate:() => {
                     return uuid.v4();
                 }
             };
